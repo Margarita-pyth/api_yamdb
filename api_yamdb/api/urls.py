d5ddf1cf51788526acc9d0ac5dd56fd7d12d3faa
Evgeny Lityushkin <tcifrovik@gmail.com>
2022-05-11 21:05:37 +0300

    router name

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -5,22 +5,22 @@ from users.views import get_jwt_token, register, UserViewSet
 from api.views import CommentViewSet, ReviewViewSet
 
 
-router = DefaultRouter()
+router_v1 = DefaultRouter()
 
-router.register(r"users", UserViewSet)
-router.register(
+router_v1.register(r"users", UserViewSet)
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
     basename='reviews'
 )
-router.register(
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
     path('v1/auth/signup/', register, name='register'),
     path('v1/auth/token/', get_jwt_token, name='token')
 ]
